95e1e4ae69f2367fecdf9661985c0027be7d629a
miguel bermud <mig.ber.bay@gmail.com>
2022-06-29 01:01:13 +0200

    a little more of a push

--- 04.ownership/src/main.rs
+++ 04.ownership/src/main.rs
@@ -7,15 +7,31 @@ pub fn scopes(){
         // do stuff with s
     }                      // this scope is now over, and s is no longer valid
     // println!("{}", s) <- this would be out of scope and wouldn't print anything.
+    // the compiler will also free the memory in the heap that belongs to s, since it went out of scope.
 }
 
-fn more(){
+fn hello_world(){
     let mut s = String::from("hello");
     s.push_str(", world!"); // push_str() appends a literal to a String
     println!("{}", s); // This will print `hello, world!`
 }
 
+fn move_var(){
+    // this first example works because integers are simple values that can be saved fully in the stack
+    let x = 5;
+    let y = x;
+    println!("{}{}",x,y);
+
+    // since ultimatelly stings are lists, they are of mutable size,
+    // and this makes it so its not borrowable if inmutable
+    // for this reason strings do not have the move operation.
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}{}",s1,s2)
+}
+
 fn main() {
     scopes();
-    more();
+    hello_world();
+    move_var();
 }
